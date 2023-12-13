36fd8d1440534bfe93ee6c10c45c4d7a5b1b07c9
Felipe Escalona <escalonaf12@gmail.com>
2023-12-12 23:54:07 -0400

    conditional

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/prop-types */
 
 export const Stats = ({ items }) => {
+    if (items.length === 0) return (<footer className="stats" > <em>🧳You have nothing on your list</em></footer>)
+
+
     const numberItems = items.length
     const packedItems = items.filter(item => item.packed === true)
     const porcentage = Math.round((packedItems.length / numberItems) * 100)
